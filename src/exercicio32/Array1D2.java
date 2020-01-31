45713ed20b975fe7cccbd7280fabab1701b206cf
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-01-31 10:08:56 -0300

    Adding more exercises that I've finished.
    32 [Loading..]
    ________________

--- src/exercicio32/Array1D2.java
+++ src/exercicio32/Array1D2.java
@@ -30,18 +30,40 @@ public class Array1D2 {
     6 3
     0 0 1 1 1 0
 
-    3 + 1 = 4 != 6, then it's NO.
+    3 + 1 = 4 != 6, then we say NO
 
     */
 
+    /* INPUT
 
+    2 <--- Number os inputs
+    5 3
+    0 0 0 0 0
 
+    6 5
+    0 0 0 1 1 1
 
-//    public static boolean canWin(int leap, int[] game) {
-//        if(leap ){
-//
-//        }
-//    }
+     */
+
+    //Q is the Leap
+    //We need to achieve to game.length
+
+    public static boolean canWin(int leap, int[] game) {
+        for (int i = 0; i < game.length; i++){
+
+            //Getting the greatest value in the array
+            int max = game[0];
+            for (int j = 1; j < game.length; j++) {
+                if (game[j] > max)
+                    max = game[j];
+            }
+            //Verify if the Max Item on the Array plus the Leap is/n't equals at the length of the whole array
+            if(max + leap != game.length){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -56,7 +78,8 @@ public class Array1D2 {
                 game[i] = scan.nextInt();
             }
 
-            //System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+            System.out.println(game.length + "<- that's the gameLength" + leap + "<- that's the leap");
         }
         scan.close();
     }
