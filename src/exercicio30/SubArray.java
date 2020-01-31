8f86be13a9017fa7ef6ff75809a486dd03b13b6f
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-01-31 09:08:15 -0300

    Adding more exercises that I've finished.
    30 [Done] <-- Finally finished =)
    31 [Done]
    32 [Loading..]
    ________________

--- src/exercicio30/SubArray.java
+++ src/exercicio30/SubArray.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class SubArray {
     public static void main(String[] args) {
 
+        //Subarray is a sequence of the array like
+
+        /*
+        Array = [1,2,3]
+        Array's subarrays = [1], [2], [3], [1,2], [2,3], [1,2,3].
+
+        !Can't skip elements and do like [1,3], the 2 element is being skipped.
+         */
+
         /*   X -->
                 0 | 1 | 2 | 3 | 4 | 5 |
              0
@@ -20,25 +29,73 @@ public class SubArray {
         //
 
         Scanner scanner = new Scanner(System.in);
-        int contagemDoValorDoArray = 0;
-        int tantosDeVezesQueSeraRepitidoOScanner = scanner.nextInt();
-        int[] a = new int[tantosDeVezesQueSeraRepitidoOScanner];
 
-        // -BASE- Popula o array ---------|
-        for (int i = 0; i < tantosDeVezesQueSeraRepitidoOScanner; i++)
-            a[i] = scanner.nextInt();
-        // -------------------------------|
+        int n = scanner.nextInt();
 
-        int contagem=0;
-        for(int c=0; c < tantosDeVezesQueSeraRepitidoOScanner; c++){
-            for(int k=c; k<tantosDeVezesQueSeraRepitidoOScanner; k++){
-                contagemDoValorDoArray=a[k]+contagemDoValorDoArray;
-                if(contagemDoValorDoArray<0){
-                    contagem++;
-                }
-            }
-            System.out.println(contagem);
+        //Initializing the Array with how many values that are being received in 'N'
+        //If put '6' in N, the array will be Int[] arr = new int[6]
+        int[] arr = new int[n];
+
+        //Filling the array with a loop
+        for(int i = 0; i < n; i++){
+            /*
+            Filling the array by the position especified in 'i', the loop will make the array pass through all the positions that was especified in 'n',
+            once I == N, it will stop.
+
+            The insert loop will be like this:
+
+            1 loop with i=1, n=4
+            arr[1] = scanner.nextInt();
+            2 loop with i=2, n=4
+            arr[2] = scanner.nextInt();
+            3 loop with i=3, n=4
+            arr[3] = scanner.nextInt();
+            4 loop with i=4, n=4
+            arr[4] = scanner.nextInt();
+            then it breaks 'cause I == N
+            */
+
+            arr[i] = scanner.nextInt();
         }
+        //Once we don't need scanner anymore we can close it.
+            scanner.close();
 
+        //Calculating the number os negative subarrays
+
+        //Calculating all the subarrays and checking which one is negative
+        /*
+        Explaning the loops:
+        Let's imagine that the input is like in the example
+        1 -2 4 -5 1
+        The 'i' index will loop through the whole array and it's gonna be like this
+        i = 1,
+        i = -2,
+        i = 4,
+        i = -5,
+        i = 1
+        */
+
+        int arraysNegativo = 0;
+        for(int i=0; i < n; i++){
+            for(int j=i; j < n; j++){
+                int somaAtual = 0;
+                for(int k=i; k <= j; k++){
+                    //somaAtual ira guardar todos os valores do array
+                    somaAtual += arr[k];
+                }
+                //Everytime that somaAtual encontrar um valor negativo no array, ArraysNegativo incrementará
+                if(somaAtual < 0){
+                    arraysNegativo++;
+                }
+
+                /*
+                I gets the first value from the array (1
+                J gets the second value (-2)
+                K will add it in a subarray form [1, -2]
+                */
+
+            }
+        }
+        System.out.println(arraysNegativo);
     }
 }
--- src/exercicio32/Array1D2.java
+++ src/exercicio32/Array1D2.java
@@ -3,11 +3,45 @@ package exercicio32;
 import java.util.Scanner;
 
 public class Array1D2 {
-    public static boolean canWin(int leap, int[] game) {
-        if(leap ){
 
-        }
-    }
+    //What to
+
+    /*
+    Ex of input
+
+    1 <---Base of how many lines will be put
+    6 <--- [6] Units that will be input in the array 5 <-- [5] is the Leap
+        ^ Explanation ->
+                        0  <-- 1
+                        0  <-- 2
+                        0  <-- 3
+                        1  <-- 4
+                        1  <-- 5
+                        1  <-- 6, so [6] six is the size of the array
+
+    0 0 0 1 1 1
+
+    So, if Leap + index = units of the end of the array
+            5   +   1   = 6
+    ^^ That's a 'YES' 'cause:
+
+    Another example:
+
+    6 3
+    0 0 1 1 1 0
+
+    3 + 1 = 4 != 6, then it's NO.
+
+    */
+
+
+
+
+//    public static boolean canWin(int leap, int[] game) {
+//        if(leap ){
+//
+//        }
+//    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -22,7 +56,7 @@ public class Array1D2 {
                 game[i] = scan.nextInt();
             }
 
-            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+            //System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
         }
         scan.close();
     }
