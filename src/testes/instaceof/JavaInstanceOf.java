53a4bb2fac8e751b7e3e046105d9aa03992a10f7
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-02-06 16:40:16 -0300

    Final commit of the day

--- src/testes/instaceof/JavaInstanceOf.java
+++ src/testes/instaceof/JavaInstanceOf.java
@@ -1,28 +1,20 @@
 package testes.instaceof;
 
-class Tree {
+class Tree { }
 
-}
+class Pine extends Tree { }
 
-class Pine extends Tree {
-
-}
-
-class Oak extends Tree {
-
-}
+class Oak extends Tree { }
 
 public class JavaInstanceOf {
     public static void main(String[] args) {
         Tree tree = new Pine();
 
-        if (tree instanceof Oak)
-            System.out.println("Oak");
-        if (tree instanceof Pine)
-            System.out.println("Pine");
-        if (tree instanceof Tree){
-            System.out.println("Tree");
-        }
+        if (tree instanceof Oak) { System.out.println("Oak"); }
+
+        //It will only be shot by this requisition 'cause Tree tree is a new Pine.
+        if (tree instanceof Pine){ System.out.println("Pine"); }
+        if (tree instanceof Tree){ System.out.println("Tree"); }
         else System.out.println("Isn't");
     }
 }
