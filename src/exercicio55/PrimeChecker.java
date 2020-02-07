f0d4072b25c643c6d456a2796ef5ca8c7d0bdc87
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-02-07 14:38:30 -0300

    Fixing older exercises

--- src/exercicio55/PrimeChecker.java
+++ src/exercicio55/PrimeChecker.java
@@ -8,7 +8,7 @@ import java.util.regex.*;
 import java.lang.reflect.*;
 import static java.lang.System.in;
 
-class Prime{
+class Prime {
 
     public static boolean isPrime(long number) {
         number = (long) Math.sqrt(number);
@@ -23,9 +23,9 @@ class Prime{
         return true;
     }
 
-    public void checkPrime(int... args){
-        for (int num: args) {
-            if(isPrime(num)){
+    public void checkPrime(int... args) {
+        for (int num : args) {
+            if (isPrime(num)) {
                 System.out.println(num + " ");
             }
         }
@@ -35,42 +35,34 @@ class Prime{
 
 public class PrimeChecker {
     public static void main(String[] args) {
-        try{
-            BufferedReader br=new BufferedReader(new InputStreamReader(in));
-            int n1=Integer.parseInt(br.readLine());
-            int n2=Integer.parseInt(br.readLine());
-            int n3=Integer.parseInt(br.readLine());
-            int n4=Integer.parseInt(br.readLine());
-            int n5=Integer.parseInt(br.readLine());
-            Prime ob=new Prime();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
             ob.checkPrime(n1);
-            ob.checkPrime(n1,n2);
-            ob.checkPrime(n1,n2,n3);
-            ob.checkPrime(n1,n2,n3,n4,n5);
-            Method[] methods=Prime.class.getDeclaredMethods();
-            Set<String> set=new HashSet<>();
-            boolean overload=false;
-            for(int i=0;i<methods.length;i++)
-            {
-                if(set.contains(methods[i].getName()))
-                {
-                    overload=true;
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
                     break;
                 }
                 set.add(methods[i].getName());
 
             }
-            if(overload)
-            {
+            if (overload) {
                 throw new Exception("Overloading not allowed");
             }
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
-
 }
-
-
