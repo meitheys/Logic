41553d225cfae73bd14722e65b0d3d57a97061d4
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-02-07 14:34:17 -0300

    Adding more exercises that I've finished.
    54 [Done]
    55 [Loading...]
    ________________

--- src/exercicio54/CanYouAcess.java
+++ src/exercicio54/CanYouAcess.java
@@ -1,4 +1,53 @@
 package exercicio54;
 
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.util.regex.*;
+import java.security.*;
+
 public class CanYouAcess {
+    public static void main(String[] args) throws Exception {
+        DoNotTerminate.forbidExit();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int num = Integer.parseInt(br.readLine().trim());
+            Object o;
+
+            o = new Inner().new Private();
+
+            //Inner is kind of a subclass inside another class
+            System.out.println(num + " is " + ((CanYouAcess.Inner.Private)o).powerof2(num));
+
+            System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+
+    static class Inner {
+        private class Private {
+            private String powerof2(int num) {
+                return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
+            }
+        }
+    }
+}
+
+class DoNotTerminate {
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+
+    public static class ExitTrappedException extends SecurityException {
+        private static final long serialVersionUID = 1L;
+    }
 }
---
+++ src/exercicio55/PrimeChecker.java
@@ -0,0 +1,76 @@
+package exercicio55;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+import static java.lang.System.in;
+
+class Prime{
+
+    public static boolean isPrime(long number) {
+        number = (long) Math.sqrt(number);
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (long i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void checkPrime(int... args){
+        for (int num: args) {
+            if(isPrime(num)){
+                System.out.println(num + " ");
+            }
+        }
+        System.out.println();
+    }
+}
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            Prime ob=new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1,n2);
+            ob.checkPrime(n1,n2,n3);
+            ob.checkPrime(n1,n2,n3,n4,n5);
+            Method[] methods=Prime.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+            for(int i=0;i<methods.length;i++)
+            {
+                if(set.contains(methods[i].getName()))
+                {
+                    overload=true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+}
+
+
